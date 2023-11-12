7999904106476fad70d5008a4cda79a16c0d31f7
Szymon Cieśla <cszymon01@gmail.com>
2023-11-12 16:47:48 +0100

    finished styling, html structure, need to add php dynamics - lazyblocks

---
+++ blocks/lazyblock-events-gallery/block.php
@@ -0,0 +1,100 @@
+<section class="events section-black">
+    <div class="container">
+        <h2 class="section-title">Minione wydarzenia</h2>
+
+        <div class="events__boxes">
+            <div class="events__box events__box-big">
+                <div class="events__box-texts">
+                    <h4 class="events__box-title">Last Minute</h4>
+                    <p class="events__box-description">29 / 09 / 2023</p>
+                </div>
+                <div class="events__box-socials">
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-instagram-alt'></i>
+                    </a>
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-facebook-circle' ></i>
+                    </a>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/event-2.jpg" alt="" class="events__box-img">
+            </div>
+            
+            <div class="events__box events__box-small">
+                <div class="events__box-texts">
+                    <h4 class="events__box-title">Last Minute</h4>
+                    <p class="events__box-description">29 / 09 / 2023</p>
+                </div>
+                <div class="events__box-socials">
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-instagram-alt'></i>
+                    </a>
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-facebook-circle' ></i>
+                    </a>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/last-minute-event.jpg" alt="" class="events__box-img">
+            </div>
+            <div class="events__box events__box-small">
+                <div class="events__box-texts">
+                    <h4 class="events__box-title">Last Minute</h4>
+                    <p class="events__box-description">29 / 09 / 2023</p>
+                </div>
+                <div class="events__box-socials">
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-instagram-alt'></i>
+                    </a>
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-facebook-circle' ></i>
+                    </a>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/last-minute-event.jpg" alt="" class="events__box-img">
+            </div>
+            <div class="events__box events__box-small">
+                <div class="events__box-texts">
+                    <h4 class="events__box-title">Last Minute</h4>
+                    <p class="events__box-description">29 / 09 / 2023</p>
+                </div>
+                <div class="events__box-socials">
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-instagram-alt'></i>
+                    </a>
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-facebook-circle' ></i>
+                    </a>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/last-minute-event.jpg" alt="" class="events__box-img">
+            </div>
+            <div class="events__box events__box-big">
+                <div class="events__box-texts">
+                    <h4 class="events__box-title">Last Minute</h4>
+                    <p class="events__box-description">29 / 09 / 2023</p>
+                </div>
+                <div class="events__box-socials">
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-instagram-alt'></i>
+                    </a>
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-facebook-circle' ></i>
+                    </a>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/event-2.jpg" alt="" class="events__box-img">
+            </div>
+             <div class="events__box events__box-small">
+                <div class="events__box-texts">
+                    <h4 class="events__box-title">Last Minute</h4>
+                    <p class="events__box-description">29 / 09 / 2023</p>
+                </div>
+                <div class="events__box-socials">
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-instagram-alt'></i>
+                    </a>
+                    <a href="" class="events__box-social">
+                        <i class='bx bxl-facebook-circle' ></i>
+                    </a>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/last-minute-event.jpg" alt="" class="events__box-img">
+            </div>
+        </div>
+        <a href="/" class="events__btn">powrót</a>
+    </div>
+</section>
